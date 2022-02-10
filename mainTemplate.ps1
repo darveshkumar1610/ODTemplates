883755a5d6ad27042c21f60aac1aca8d6253050c
Darvesh Kumar <darveshkumar1610@gmail.com>
2022-02-10 13:11:36 +0400

    Update mainTemplate.ps1

--- mainTemplate.ps1
+++ mainTemplate.ps1
@@ -1,9 +1,65 @@
-$AKS_NAME="od-aks-cluster"
-$LOCATION="westeurope"
-$NODE_SIZE="Standard_B2s"
-$DEPLOYMENT_NAME="$AKS_NAME"+ "-" +"$RANDOM"
-$DNS_PREFIX="od-aks-dns"
+{
+    "$schema": "https://schema.management.azure.com/schemas/2019-08-01/tenantDeploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {},
+    "functions": [],
+    "variables": {},
+    "resources": [
+        {
+            "name": "cosmosDBDeployment",
+            "type": "Microsoft.Resources/deployments",
+            "apiVersion": "2021-04-01",
+            "properties": {
+                "mode": "Incremental",
+                "templateLink": {
+                    "uri" : "https://raw.githubusercontent.com/ClearPeaksWeb/od-marketplace-arm-templates/main/cosmosTemplate.json?token=AXB6QKMD7IJUWIHMGX6R3PTB2LLDI",
+                    "contentVersion": "1.0.0.0"
+                },
+                "parameters": {}
+            }
+        },
 
-New-AzResourceGroupDeployment -name $DEPLOYMENT_NAME`
--TemplateFile "./kubernetes.json" `
--TemplateParameterFile "C:\\Users\\VinayDwivedi\\OneDrive - Clear Peaks SL\\Escritorio\\abc\\parameter.json"
+        {
+            "name": "containerRegistryDeployment",
+            "type": "Microsoft.Resources/deployments",
+            "apiVersion": "2021-04-01",
+            "properties": {
+                "mode": "Incremental",
+                "templateLink": {
+                    "uri" : "https://raw.githubusercontent.com/ClearPeaksWeb/od-marketplace-arm-templates/main/containerRegistryTemplate.json?token=AXB6QKLPEX2VXYNWZDLMNZDB2LLAQ",
+                    "contentVersion": "1.0.0.0"
+                },
+                "parameters": {}
+            }
+        },
+
+        {
+            "name": "postgreSqlDBDeployment",
+            "type": "Microsoft.Resources/deployments",
+            "apiVersion": "2021-04-01",
+            "properties": {
+                "mode": "Incremental",
+                "templateLink": {
+                    "uri" : "https://raw.githubusercontent.com/ClearPeaksWeb/od-marketplace-arm-templates/main/postgresqltemplate.json?token=AXB6QKJEONAV3VZQUITBWULB2LLHS",
+                    "contentVersion": "1.0.0.0"
+                },
+                "parameters": {}
+            }
+        },
+
+        {
+            "name": "kubernetesDeployment",
+            "type": "Microsoft.Resources/deployments",
+            "apiVersion": "2021-04-01",
+            "properties": {
+                "mode": "Incremental",
+                "templateLink": {
+                    "uri" : "https://raw.githubusercontent.com/ClearPeaksWeb/od-marketplace-arm-templates/main/kubernetes.json?token=AXB6QKM4U75PD7IYOFVSIO3B2LLJG",
+                    "contentVersion": "1.0.0.0"
+                },
+                "parameters": {}
+            }
+        }
+    ],
+    "outputs": {}
+}
